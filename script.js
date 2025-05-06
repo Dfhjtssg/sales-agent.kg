d571f066de5540473cd1d2285c4e020554561199
Штирлиц <152861800+Dfhjtssg@users.noreply.github.com>
2025-05-06 22:31:24 +0600

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("changeColorButton").addEventListener("click", function () {
-    const colors = ["#f4f4f4", "#ffcccb", "#d1ffd6", "#d6e7ff", "#fdfd96"];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.body.style.backgroundColor = randomColor;
-});
